a9a4262c423daf9b28b05797fa2d75dbcee8a006
VladSash <115373009+VladSash@users.noreply.github.com>
2022-10-15 18:35:59 +0300

    Add files via upload

---
+++ sintaksis 1-2/index.js
@@ -0,0 +1,21 @@
+// JavaScript-код с созданием переменных
+
+/*  
+    Автор
+    Влад Сашков
+*/
+
+const myName="Влад"
+console.log(myName)
+
+const mySurname="Сашков"
+console.log(mySurname)
+
+let myFavoriteDrink="Молочный улун"
+console.log(myFavoriteDrink)
+
+let myFavoriteAnimal="Хомяк Пирожок"
+console.log(myFavoriteAnimal)
+
+let myFavoriteProgrammingLanguage="JavaScript"
+console.log(myFavoriteProgrammingLanguage)
